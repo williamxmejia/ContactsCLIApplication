178cb0df5ec914b518bfed74f0ad5c2427127c2b
William Mejia <willx1995@gmail.com>
2022-04-12 23:55:43 -0500

    refactoring code

--- src/contacts.java
+++ src/contacts.java
@@ -16,12 +16,37 @@ public class contacts {
         this.phoneNumber = phoneNumber;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public String toString() {
         return this.firstName + " " + this.lastName + " " + this.phoneNumber;
     }
 
 
     public static void main(String[] args) throws IOException {
+        //Paths
         String myPath = "/Users/williammejia/Documents/personal-java-projects/src/Data/contacts.txt";
         String fileName = "contacts.txt";
         String directory = "data";
@@ -29,105 +54,28 @@ public class contacts {
         Scanner sc = new Scanner(System.in);
 
         Path oneDirectoryPath = Paths.get("data");
-
         Path contactsPath = Paths.get("data", "contacts.txt");
         Path contactsDirPath = Paths.get("data");
 
-//        System.out.println(dashes("1231231234"));
-
+        //Logic
         List<String> contacts;
         while(true){
-            System.out.println("1. View contacts");
-            System.out.println("2. Add a new contacts");
-            System.out.println("3. Search a contact by name");
-            System.out.println("4. Delete an existing contact");
-            System.out.println("5. Exit");
-            System.out.println("Enter an option(1, 2, 3, 4, or 5):");
+            contactsManager.mainMenu();
             int view = sc.nextInt();
-        if (view == 1) {
-            try {
-                if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
-                if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
-                contacts = Files.readAllLines(contactsPath);
-                System.out.println("Name | Number");
-                System.out.printf("%s\n", "-------------");
-                for(int i = 0; i < contacts.size(); i++){
-                    System.out.println(contacts.get(i));
-                }
-                System.out.println("--------------");
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.println("Cannot Read " + fileName);
-            }
+            if (view == 1) {
+                contactsManager.readFiles(contactsPath, contactsDirPath, filePath, fileName);
         }
 
         if (view == 2) {
-            try {
-                if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
-                if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
-                System.out.println("First name: ");
-                String firstName = sc.next();
-                System.out.println("Last name");
-                String lastName = sc.next();
-                System.out.println("Phone number");
-                String phoneNumber = sc.next();
-                String phone = phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3");
-                List<String> items = Arrays.asList(firstName + " " + lastName + " | " + phone);
-                Files.write(filePath, items, StandardOpenOption.APPEND);
-            } catch (IOException e) {
-                System.out.println("Error writing " + fileName);
-            }
+            contactsManager.writeFiles(contactsPath, contactsDirPath, filePath, fileName);
         }
 
         if(view == 3){
-            if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
-            if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
-            System.out.println("Retrieve contacts by name and/or phone number. ");
-            System.out.print(" Name or Number: ");
-            Scanner scanName = new Scanner(System.in);
-            String searched = scanName.nextLine();
-            Path ContactsPath = Paths.get("data", "contacts.txt");
-            List<String> items;
-            try {
-                items = Files.readAllLines(ContactsPath);
-                for (String person : items) {
-                    if (person.toLowerCase().contains(searched.toLowerCase())) {
-                        System.out.println("Contact:\n" + person);
-                        System.out.println("--------------");
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            contactsManager.search(contactsPath, contactsDirPath, filePath, fileName);
         }
 
         if(view == 4){
-            if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
-            if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
-            System.out.println("Delete contacts by name and/or phone number.");
-            System.out.println(" Name or Number: ");
-            Scanner scanDelete = new Scanner(System.in);
-            String searched = scanDelete.nextLine();
-            Path ContactsPath = Paths.get("data", "contacts.txt");
-            List<String> items;
-            try{
-                items = Files.readAllLines(ContactsPath);
-                List<String> revisedList = new ArrayList<>();
-                for (String person : items){
-                    if (person.toLowerCase().contains(searched.toLowerCase())) {
-                        continue;
-                    }
-                    revisedList.add(person);
-                }
-                for(String revised : revisedList){
-                    System.out.println(revised);
-                }
-                Files.write(contactsPath, revisedList);
-            }catch (IOException e){
-                e.printStackTrace();
-                System.out.println("Cannot erase list item.");
-            }
-
+            contactsManager.search(contactsPath, contactsDirPath, filePath, fileName);
         }
 
         if(view == 5){
--- src/contactsManager.java
+++ src/contactsManager.java
@@ -1,4 +1,3 @@
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,16 +6,103 @@ import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class contactsManager {
+    public static void mainMenu(){
+        System.out.println("1. View contacts");
+        System.out.println("2. Add a new contacts");
+        System.out.println("3. Search a contact by name");
+        System.out.println("4. Delete an existing contact");
+        System.out.println("5. Exit");
+        System.out.println("Enter an option(1, 2, 3, 4, or 5):");
+    }
 
-//    public static int mainMenu(){
-//        System.out.println("1. View contacts");
-//        System.out.println("2. Add a new contacts");
-//        System.out.println("3. Search a contact by name");
-//        System.out.println("4. Delete an existing contact");
-//        System.out.println("5. Delete and existing contact");
-//        System.out.println("Enter an option(1, 2, 3, 4, or 5):");
-//        return 0;
-//    }
+    public static void readFiles(Path contactsPath, Path contactsDirPath, Path filePath, String fileName){
+        try {
+            if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
+            if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
+            List<String> contacts = Files.readAllLines(contactsPath);
+            System.out.println("Name | Number");
+            System.out.printf("%s\n", "-------------");
+            for(int i = 0; i < contacts.size(); i++){
+                System.out.println(contacts.get(i));
+            }
+            System.out.println("--------------");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Cannot Read " + fileName);
+        }
+    }
+
+    public static void writeFiles(Path contactsPath, Path contactsDirPath, Path filePath, String fileName){
+        try {
+            Scanner sc = new Scanner(System.in);
+            if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
+            if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
+            System.out.println("First name: ");
+            String firstName = sc.next();
+            System.out.println("Last name");
+            String lastName = sc.next();
+            System.out.println("Phone number");
+            String phoneNumber = sc.next();
+            String phone = phoneNumber.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3");
+            List<String> items = Arrays.asList(firstName + " " + lastName + " | " + phone);
+            Files.write(filePath, items, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            System.out.println("Error writing " + fileName);
+        }
+    }
+
+    public static void search(Path contactsPath, Path contactsDirPath, Path filePath, String fileName){
+        Scanner scanName = new Scanner(System.in);
+        try {
+            if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
+            if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
+            System.out.println("Retrieve contacts by name and/or phone number. ");
+            System.out.print(" Name or Number: ");
+            String searched = scanName.nextLine();
+            Path ContactsPath = Paths.get("data", "contacts.txt");
+            List<String> items;
+                items = Files.readAllLines(ContactsPath);
+                for (String person : items) {
+                    if (person.toLowerCase().contains(searched.toLowerCase())) {
+                        System.out.println("Contact:\n" + person);
+                        System.out.println("--------------");
+                    }
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(Path contactsPath, Path contactsDirPath, Path filePath, String fileName){
+        Scanner scanName = new Scanner(System.in);
+        try{
+            if (Files.notExists(contactsPath)) Files.createDirectories(contactsDirPath);
+            if (Files.notExists(contactsPath)) Files.createFile(contactsPath);
+            System.out.println("Delete contacts by name and/or phone number.");
+            System.out.println(" Name or Number: ");
+            Scanner scanDelete = new Scanner(System.in);
+            String searched = scanDelete.nextLine();
+            Path ContactsPath = Paths.get("data", "contacts.txt");
+            List<String> items;
+                items = Files.readAllLines(ContactsPath);
+                List<String> revisedList = new ArrayList<>();
+                for (String person : items){
+                    if (person.toLowerCase().contains(searched.toLowerCase())) {
+                        continue;
+                    }
+                    revisedList.add(person);
+                }
+                for(String revised : revisedList){
+                    System.out.println(revised);
+                }
+                Files.write(contactsPath, revisedList);
+            }catch (IOException e){
+                e.printStackTrace();
+                System.out.println("Cannot erase list item.");
+        }
+
+    }
 }
